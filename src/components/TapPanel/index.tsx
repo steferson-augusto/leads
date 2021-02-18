ffb4adc03d0a67c7a5aa279ec1235fc2206c1ebc
Steferson Augusto <steferson_a@hotmail.com>
2021-02-17 23:06:18 -0400

    feat: cria componente para painel tab

---
+++ src/components/TapPanel/index.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { Box } from '@material-ui/core'
+
+interface TabPanelProps {
+  children?: React.ReactNode
+  index: number
+  value: number
+}
+
+const TabPanel: React.FC<TabPanelProps> = ({ children, value, index }) => {
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`tabpanel-${index}`}
+      aria-labelledby={`tab-${index}`}
+    >
+      {value === index && <Box p={3}>{children}</Box>}
+    </div>
+  )
+}
+
+export default TabPanel
