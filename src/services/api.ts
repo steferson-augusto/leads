4fcac3ece9da81c95d31c159b091fe6ae6c83cc0
Steferson Augusto <steferson_a@hotmail.com>
2021-02-17 23:12:12 -0400

    feat: adiciona token jwt as requisições

--- src/services/api.ts
+++ src/services/api.ts
@@ -6,4 +6,7 @@ const api = axios.create({
   baseURL
 })
 
+api.defaults.headers.Authorization =
+  'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJBdXRob3JpemF0aW9uSWQiOiJjMzQ1YjY1Ny0wYjBjLTExZWItOThmMy0wMGZmNjU0YmQzN2EiLCJVc2VySWQiOiIxIiwicm9sZSI6IlN5c3RlbU93bmVyIiwibmJmIjoxNjAyMzQzODY3LCJleHAiOjE2MzM4Nzk4NjcsImlhdCI6MTYwMjM0Mzg2N30.cZV7gxe0iOtHX2f8URK8aOEvTbdybuAm_Tlu_LueB5U'
+
 export default api
